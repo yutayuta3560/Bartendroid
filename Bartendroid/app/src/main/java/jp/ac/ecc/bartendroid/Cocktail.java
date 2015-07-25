f4987a2ef2eeef6fc17b203e5a3083816cfc47a7
Sakamoto <sakamoto144yuta@gmail.com>
2015-07-25 13:22:09 +0900

    マージ後の調節

--- Bartendroid/app/src/main/java/jp/ac/ecc/bartendroid/Cocktail.java
+++ Bartendroid/app/src/main/java/jp/ac/ecc/bartendroid/Cocktail.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package jp.ac.ecc.bartendroid;
 
 import java.util.ArrayList;
@@ -30,36 +29,3 @@ public class Cocktail {
     }
     
 }
-=======
-package jp.ac.ecc.bartendroid;
-
-import java.util.ArrayList;
-
-public class Cocktail {
-    private String m_cocktail_name;
-    private final ArrayList<Material> m_material_list;
-    
-    public Cocktail(String name) {
-        super();
-        m_material_list = new ArrayList<Material>();
-        m_cocktail_name = name;
-    }
-    
-    public void setCocktailName(String name) {
-        m_cocktail_name = name;
-    }
-    
-    public String getCocktailName() {
-        return m_cocktail_name;
-    }
-    
-    public void addMaterial(Material material) {
-        m_material_list.add(material);
-    }
-    
-    public ArrayList<Material> getMaterial() {
-        return m_material_list;
-    }
-    
-}
->>>>>>> 684c3f6193e1357a65cc340a8ad0d5936f3ddc56
--- Bartendroid/app/src/main/java/jp/ac/ecc/bartendroid/Material.java
+++ Bartendroid/app/src/main/java/jp/ac/ecc/bartendroid/Material.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package jp.ac.ecc.bartendroid;
 
 public class Material {
@@ -68,74 +67,3 @@ public class Material {
         this.m_shibumi = shibumi;
     }    
 }
-=======
-package jp.ac.ecc.bartendroid;
-
-public class Material {
-
-    private String m_material_name;
-    
-    private int m_sweetness;
-    private int m_clear;
-    private int m_bitterness;
-    private int m_sourness;
-    private int m_shibumi;
-
-    public Material (String name, int sweetness, int clear, int bitter, int sour, int shibumi) {
-        this.m_material_name = name;
-        this.m_sweetness = sweetness;
-        this.m_clear = clear;
-        this.m_bitterness = bitter;
-        this.m_sourness = sour;
-        this.m_shibumi = shibumi;
-    }
-    
-    public String getMaterialName() {
-        return m_material_name;
-    }
-
-    public void setMaterialName(String material_name) {
-        this.m_material_name = material_name;
-    }
-
-    public int getSweetness() {
-        return m_sweetness;
-    }
-
-    public void setSweetness(int sweetness) {
-        this.m_sweetness = sweetness;
-    }
-
-    public int getClear() {
-        return m_clear;
-    }
-
-    public void setClear(int clear) {
-        this.m_clear = clear;
-    }
-
-    public int getBitterness() {
-        return m_bitterness;
-    }
-
-    public void setBitterness(int bitterness) {
-        this.m_bitterness = bitterness;
-    }
-
-    public int getSourness() {
-        return m_sourness;
-    }
-
-    public void setSourness(int sourness) {
-        this.m_sourness = sourness;
-    }
-
-    public int getShibumi() {
-        return m_shibumi;
-    }
-
-    public void setShibumi(int shibumi) {
-        this.m_shibumi = shibumi;
-    }    
-}
->>>>>>> 684c3f6193e1357a65cc340a8ad0d5936f3ddc56
