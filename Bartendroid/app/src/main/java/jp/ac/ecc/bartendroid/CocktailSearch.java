84e679fc53dbc7198cbc83a4d56fc9867ba97ac6
矢部雄大 <katsu.yudai@gmail.com>
2015-07-29 06:09:08 +0900

    Searchのレイアウト　ひと通り完成

--- Bartendroid/app/src/main/java/jp/ac/ecc/bartendroid/CocktailSearch.java
+++ Bartendroid/app/src/main/java/jp/ac/ecc/bartendroid/CocktailSearch.java
@@ -2,23 +2,67 @@ package jp.ac.ecc.bartendroid;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CocktailSearch extends ActionBarActivity {
 
+    private RadioGroup group;
+    private RadioButton alcoholeButton;
+    private RadioButton nonalcoholeButton;
+    private Spinner basespinner;
+    private Spinner tastespinner1;
+    private Spinner tasetelevelspinner1;
+    private Button searchbutton;
+    private ListView searchListView;
+    private CocatailDB db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        db = new CocatailDB(getApplicationContext());
         setContentView(R.layout.activity_cocktail_search);
+        setView();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_cocktail_search, menu);
-        return true;
+
+    private void setView(){
+        group = (RadioGroup)findViewById(R.id.alcoholegroup);
+        alcoholeButton = (RadioButton)findViewById(R.id.alcoholebutton);
+        nonalcoholeButton = (RadioButton)findViewById(R.id.nonalcoholebutton);
+        basespinner = (Spinner)findViewById(R.id.basespinner);
+        tastespinner1 = (Spinner)findViewById(R.id.tastespiner1);
+        tasetelevelspinner1 = (Spinner)findViewById(R.id.tastelevelspiner1);
+        searchbutton = (Button)findViewById(R.id.searchButton);
+        searchListView = (ListView)findViewById(R.id.searchListView);
+
+    }
+    private void setSpinnerAdapter(){
+        ArrayList<Material> materials = db.getMaterials();
+        List<String> baselist = new ArrayList<>();
+        for(Material material : materials){
+            baselist.add(material.getMaterialName());
+        }
+        basespinner.setAdapter(createSpinnerAdapter(baselist));
+
+    }
+    private ArrayAdapter<String> createSpinnerAdapter(List<String> list){
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item);
+        for(String item : list){
+            adapter.add(item);
+        }
+
+        return adapter;
     }
 
     @Override
